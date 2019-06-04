9624722a77cc1f337ec0b44b14552c844e1d4f40
Eric Storey <estorey@gmail.com>
2019-06-04 17:36:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,11 @@ class SessionsController < ApplicationController
   end
   
   def create
-    session[:name] = params[:name]
-    redirect_to '/'
+    if params[:name]==""
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
   end 
   
   def destroy
